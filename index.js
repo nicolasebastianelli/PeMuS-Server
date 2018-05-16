b87b700b2222bae87dc10b6da1852a05848e5b62
Nicola <nicola.sebastianelli@studio.unibo.it>
2018-05-16 18:53:44 +0200

    heroku settings 3

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express();
 
-var port=3000;
+var port=process.env.PORT || 3000;
 
-var srv = app.listen(process.env.PORT || 3000, function() {
+var srv = app.listen(port, function() {
     console.log('Listening on '+port)
 });
 
